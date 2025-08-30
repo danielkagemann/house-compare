c73193db1ee26501893da14d03d6895a4f692193
Daniel Kagemann <info@danielkagemann.name>
2025-08-30 06:21:42 +0200

    updated basepath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/house-compare", // required for github pages to load the image assets correct
   output: "export",
 };
 
