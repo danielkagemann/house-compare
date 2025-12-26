4b0db2db033aff8887a6b04428705107aa4202e2
Daniel Kagemann <info@danielkagemann.name>
2025-12-26 13:09:44 +0100

    feat(amenities): sorted by distance

--- src/components/list-of-amenities.tsx
+++ src/components/list-of-amenities.tsx
@@ -1,10 +1,26 @@
 import { amenityTitle, distanceBetweenCoordinates, Location } from "@/model/Listing";
+import { useCallback } from "react";
 
 interface ListOfAmenitiesProps {
    item: Location;
 }
+
+type DisplayPOI = {
+   name: string;
+   distance: number;
+}
+
 export const ListOfAmenities = ({ item }: ListOfAmenitiesProps) => {
    const types = Array.from(new Set(item.poi?.map(poi => poi.type)));
+
+   const amenitiesForType = useCallback((type: string): DisplayPOI[] => {
+      return item.poi!.filter(poi => poi.type === type)
+         .map((poi) => ({
+            name: poi.name || 'unbekannt',
+            distance: distanceBetweenCoordinates(item, poi.coordinates)
+         }))
+         .sort((a, b) => a.distance - b.distance);
+   }, [item]);
 
    if (item.poi === undefined || item.poi.length === 0) {
       return <p>Keine Sehenswürdigkeiten in der Nähe gefunden.</p>;
@@ -17,8 +33,8 @@ export const ListOfAmenities = ({ item }: ListOfAmenitiesProps) => {
                <div key={idx} className="mb-2">
                   <strong>{amenityTitle(type)}</strong>
                   <div className="bg-gray-100 rounded p-2 flex flex-col">
-                     {item.poi!.filter(poi => poi.type === type).map((poi, pidx) => (
-                        <div key={pidx}>{poi.name || 'unbekannt'} <strong>{distanceBetweenCoordinates(item, poi.coordinates).toFixed(2)}km</strong> entfernt</div>
+                     {amenitiesForType(type).map((amenity, aidx) => (
+                        <div key={aidx}>{amenity.name} <strong>{amenity.distance.toFixed(2)}km</strong> entfernt</div>
                      ))}
                   </div>
                </div>
